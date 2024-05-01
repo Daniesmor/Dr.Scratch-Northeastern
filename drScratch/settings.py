7e63d4acce88725403db41cdbe4f154d6be268c1
daniel <daniesmor@gmail.com>
2024-05-01 12:08:44 +0200

    django captcha dependency remvoed

--- drScratch/settings.py
+++ drScratch/settings.py
@@ -42,7 +42,6 @@ CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(',')
 
 INSTALLED_APPS = (
     'app',
-    'django_recaptcha',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
